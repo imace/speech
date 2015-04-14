ae0ac5f033365f1e6d953858488c6b2924e4d8a9
iothz <iothouzhuo@outlook.com>
2015-04-14 22:52:44 +0800

    sliding menu

--- Note2/src/com/example/note2/MainActivity.java
+++ Note2/src/com/example/note2/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.note2;
 
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.note2.db.NotesDB;
+import com.example.note2.ContentFragment;
+import com.example.note2.R;
 
 public class MainActivity extends ListActivity {
+	
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawList;
+	private ArrayList<String> menulist;
+	private ArrayAdapter<String> menuAdapter;
+
 	
 	
 
@@ -26,6 +40,28 @@ public class MainActivity extends ListActivity {
 		@Override
 		public void onClick(View v) {
 			startActivityForResult(new Intent(MainActivity.this, AtyEditNote.class), REQUEST_CODE_ADD_NOTE);
+		}
+	};
+	private OnItemClickListener btnMenuList_clickHandler = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			/*
+			 * ¶¯Ì¬²åÈëÒ»¸öfragementµ½frameLayoutÖÐ
+			 */
+			
+			Fragment contentFragment = new ContentFragment();
+			Bundle args = new Bundle();
+			args.putString("text", menulist.get(position));
+			contentFragment.setArguments(args);
+
+			FragmentManager fm = getFragmentManager();
+			fm.beginTransaction().replace(R.id.content_frame, contentFragment)
+					.commit();
+
+			mDrawerLayout.closeDrawer(mDrawList);
+			
 		}
 	};
 	
@@ -46,6 +82,26 @@ public class MainActivity extends ListActivity {
 		refreshNotesListView();
 		
 		findViewById(R.id.btnAddNote).setOnClickListener(btnAddNote_clickHandler);
+		
+/*
+ * ²à±ßÀ¸²¿·Ö		
+ */
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawList = (ListView) findViewById(R.id.left_drawer);
+		
+		menulist = new ArrayList<String>();
+		menulist.add("登陆");
+		menulist.add("上传");
+		menulist.add("下载");
+		menulist.add("共享");
+		menulist.add("反馈");
+		
+		menuAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,menulist);
+		mDrawList.setAdapter(menuAdapter);
+		mDrawList.setOnItemClickListener(btnMenuList_clickHandler );
+		
+		
+		
 		
 	}
 	
@@ -91,26 +147,11 @@ public class MainActivity extends ListActivity {
 	}
 	
 
-	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
 	}
 	
 	
